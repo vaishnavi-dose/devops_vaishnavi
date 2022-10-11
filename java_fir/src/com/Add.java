20209b784bd56e70296219d0555ae79e4f3aa7db
vaishnavi-dose <114053692+vaishnavi-dose@users.noreply.github.com>
2022-10-11 11:13:55 +0530

    Commit

---
+++ java_fir/src/com/Add.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Add 
+{
+	int addition (int a, int b) {
+		return a+b;
+	}
+	public static void main(String args[])
+	{
+		Add a = new Add();
+		a.addition(5, 5);
+	}
+}
